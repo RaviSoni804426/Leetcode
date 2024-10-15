026ea5caae547f93caa3841a3cd7b9115a48848d
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-10-15 13:29:55 +0530

    Time: 76 ms (61.89%), Space: 70.7 MB (20.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,8 +3,9 @@ public:
     int maxSubArray(vector<int>& nums) {
         int k = 0;
         int maxSub = nums[0];
-        for(int i = 0; i < nums.size(); i++) {
-            if(k < 0) k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (k < 0)
+                k = 0;
             k += nums[i];
             maxSub = max(maxSub, k);
         }
